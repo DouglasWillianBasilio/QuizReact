e054bb46f70951224090a5923346fce00c344060
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-04-13 19:08:30 -0300

    validando questions

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -10,6 +10,13 @@ const Question = () => {
     const [quizState, dispatch] = useContext(QuizContext)
     const currentQuestion = quizState.questions[quizState.currentQuestion]
 
+    const onSelectOption = (option) => {
+        dispatch({
+            type: "CHECK_ANSWER", 
+            payload: {answer: currentQuestion.answer, option },
+        })
+    }
+
 
     return (
     <div id="question">
@@ -17,10 +24,16 @@ const Question = () => {
         <h2>{currentQuestion.question}</h2>
         <div id="options-container">
             {currentQuestion.options.map((option) => (
-                <Option option={option} key={option}/>
+                <Option option={option} 
+                key={option}
+                answer={currentQuestion.answer}
+                selectOption={() => onSelectOption(option)}
+                 />
             ))}
         </div>
-        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
+        {quizState.answerSelected && (
+            <button onClick={() => dispatch ({ type:"CHANGE_QUESTION"})}>Continuar </button>
+        )}
     </div>
     );
 }
--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -7,11 +7,11 @@ const initialState = {
     gameStage: STAGES[0],
     questions,
     currentQuestion: 0,
-    score: 0
+    score: 0,
+    answerSelected: false,
 }
 
 const quizReducer = (state, action) => {
-    console.log(state, action);
     switch(action.type) {
         case "CHANGE_STATE":
             return {
@@ -45,6 +45,21 @@ const quizReducer = (state, action) => {
 
             case "NEW_GAME":
                 return initialState;
+
+            case "CHECK_ANSWER":
+                if(state.answerSelected) return state;
+
+                const answer = action.payload.answer
+                const option = action.payload.option
+                let correctAnswer = 0
+
+                if(answer === option) correctAnswer = 1;
+
+                return {
+                    ...state,
+                    score: state.score + correctAnswer,
+                    answerSelected: option,
+                }
 
             default:
                 return state;
